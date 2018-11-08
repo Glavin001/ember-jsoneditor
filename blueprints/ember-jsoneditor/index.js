05214ae8e55704fcc6a75275621ad76256679b37
maxwondercorn <gmartell@pobox.com>
2018-11-07 22:01:26 -0500

    fixed missed bower install in blueprint

--- blueprints/ember-jsoneditor/index.js
+++ blueprints/ember-jsoneditor/index.js
@@ -1,7 +1,3 @@
 module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
-
-  afterInstall: function() {
-    return this.addBowerPackageToProject('jsoneditor'); // is a promise
-  }
 };
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    // app.import(app.bowerDirectory + '/jsoneditor/dist/jsoneditor.js');
     app.import('node_modules/jsoneditor/dist/jsoneditor.css');
     app.import('node_modules/jsoneditor/dist/img/jsoneditor-icons.svg', {
       destDir: 'assets/img'
