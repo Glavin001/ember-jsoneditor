aab05a90dffc8dcf431e29c85178a22f2e17a568
maxwondercorn <gregg.martell@gmail.com>
2018-11-08 04:46:52 -0500

    Broccolli (#20)
    * converted app.import to broccoli so will work with cli < 2.16
    
    * 0.6.0

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-'use strict';
+"use strict";
+var path = require("path");
+var Funnel = require("broccoli-funnel");
+var MergeTrees = require("broccoli-merge-trees");
 
 module.exports = {
-  name: require('./package').name,
+  name: require("./package").name,
+
   included: function(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('node_modules/jsoneditor/dist/jsoneditor.css');
-    app.import('node_modules/jsoneditor/dist/img/jsoneditor-icons.svg', {
-      destDir: 'assets/img'
+    app.import("vendor/jsoneditor.css");
+    app.import("vendor/jsoneditor-icons.svg", {
+      destDir: "assets/img"
     });
+  },
 
+  treeForVendor(vendorTree) {
+    let cssTree = new Funnel(
+      path.join(this.project.root, "node_modules", "jsoneditor/dist"),
+      {
+        files: ["jsoneditor.css"]
+      }
+    );
+
+    let iconTree = new Funnel(
+      path.join(this.project.root, "node_modules", "jsoneditor/dist/img"),
+      {
+        files: ["jsoneditor-icons.svg"]
+      }
+    );
+
+    return new MergeTrees([vendorTree, cssTree, iconTree]);
   }
 };
